6f8c201efc868a43bcc6979386c5efe854d0789f
dadifr <dav.calise@stud.uniroma3.it>
2025-03-26 19:46:25 +0100

    Update LabirintoTest.java

--- test/LabirintoTest.java
+++ test/LabirintoTest.java
@@ -4,11 +4,34 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class LabirintoTest {
+public class LabirintoTest {
+	Labirinto 1;
+	Stanza biblioteca;
+	Stanza DS1;
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
+	@Before
+	public void setUp() {
+		1 = new Labirinto();
+		1.creaStanze();
+		biblioteca = new Stanza("Biblioteca");
+		DS1 = new Stanza("DS1");
 	}
 
+	
+	@Test
+	public void testGetStanzaVincente() {
+		assertEquals("biblioteca", 1.getStanzaVincente().getNome());
+	}
+
+	@Test
+	public void testSetStanzaCorrente(){
+		1.steStanzaCorrente(DS1);
+		assertEquals(DS1, 1.getStanzaCorrente());
+	}
+
+	@Test
+	public void testGetStanzaCorrente(){
+		assertEquals("Atrio", 1.getStanzaCorrente().getNome());
+	}
+	
 }
