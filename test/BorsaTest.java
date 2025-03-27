3bc2af828094ec8aab9530f7fbf08d5ae30ebd2d
dadifr <dav.calise@stud.uniroma3.it>
2025-03-27 16:43:04 +0100

    Update BorsaTest.java

--- test/BorsaTest.java
+++ test/BorsaTest.java
@@ -1,1 +1,37 @@
+package test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class BorsaTest {
+
+	Borsa b = new Borsa();
+	Attrezzo torcia;
+	Attrezzo ascia;
+	
+	@Before
+	public void setUp() {
+		falce = new Attrezzo("torcia", 2);
+		sega = new Attrezzo("ascia", 10);
+	}
+
+	@Test
+	public void testAddAttrezzoPesoMinoreDiDieci() {
+		assertTrue(b.addAttrezzo(torcia));
+
+	}
+	
+	@Test
+	public void testAddAttrezzoPesoMaggioreDiDieci() {
+		assertFalse(b.addAttrezzo(ascia));
+
+	}
+	
+	@Test
+	public void testGetPeso() {
+		b.addAttrezzo(torcia);
+		assertEquals(torcia, b.getAttrezzo("torcia"));
+
+	}
+}
