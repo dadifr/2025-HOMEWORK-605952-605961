9b30d410ef3b317229ef76dc173b7eaa167454c2
dadifr <dav.calise@stud.uniroma3.it>
2025-03-27 16:18:13 +0100

    Update StanzaTest.java

--- test/StanzaTest.java
+++ test/StanzaTest.java
@@ -1,1 +1,28 @@
+package test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class StanzaTest() {
+  Stanza s1 = new Stanza("s1");
+  Stanza s2 = new Stanza("s2");
+  Attrezzo t = new Attrezzo("torcia", 2);
+
+  @Test
+    public void testGetStanzaAdiacente(){
+    assertNull(s1.getStanzaAdiacente("nord"));
+  }
+
+  @Test
+    public void testImpostaStanzaAdiacente(){
+    s1.impostaStanzaAdiacente("nord", s2);
+    assertEquals(s2, s1.getStanzaAdiacente("nord"));
+  }
+
+  @Test
+    public void testAttrezzo(){
+    assertTrue(s1.addAttrezzo(t));
+  }
+  
+}
