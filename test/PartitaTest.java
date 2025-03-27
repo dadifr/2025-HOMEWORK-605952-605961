266df9071d9d246ac9212f8c332d0e4b3d5eb308
dadifr <dav.calise@stud.uniroma3.it>
2025-03-27 16:10:19 +0100

    Update PartitaTest.java

--- test/PartitaTest.java
+++ test/PartitaTest.java
@@ -1,1 +1,26 @@
+package test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class PartitaTest() {
+  Partita p = new Partita();
+  Stanza s = new Stanza("Stanza");
+
+  @Test
+    public void testGetStanzaVincente(){
+    assertEquals("Biblioteca", p.getLabirinto().getStanzaVincente().getNome());
+}
+
+  @Test
+    public void testStanzaCorrente(){
+    p.getLabirinto().setStanzaCorrente(s);
+    assertEquals(s, p.getLabirinto().getStanzaCorrente());
+  }
+
+  @Test
+    public void testIsFinita(){
+    assertFalse(p.isFinita());
+  }
+}
