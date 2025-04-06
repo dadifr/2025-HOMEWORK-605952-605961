de60dae4cd9a236c7c65f8be91d8c0c171069b14
dadifr <dav.calise@stud.uniroma3.it>
2025-04-06 16:42:44 +0200

    Update StanzaTest.java

--- test/StanzaTest.java
+++ test/StanzaTest.java
@@ -1,8 +1,11 @@
-package test;
+package it.uniroma3.diadia.ambienti;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import it.uniroma3.diadia.attrezzi.*;
 
 public class StanzaTest() {
   Stanza s1 = new Stanza("s1");
