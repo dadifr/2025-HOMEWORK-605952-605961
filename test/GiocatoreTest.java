f5b57726e78267de116edc918c5be5174421ef94
dadifr <dav.calise@stud.uniroma3.it>
2025-04-06 16:42:00 +0200

    Update GiocatoreTest.java

--- test/GiocatoreTest.java
+++ test/GiocatoreTest.java
@@ -1,9 +1,9 @@
-package test;
+package it.uniroma3.diadia.giocatore;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 public class GiocatoreTest {
 	
 	Giocatore g = new Giocatore();
