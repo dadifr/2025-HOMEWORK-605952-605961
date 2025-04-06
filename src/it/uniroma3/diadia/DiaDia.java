5956b58ba91e39287deb10335dd2fb19db6cf49f
dadifr <dav.calise@stud.uniroma3.it>
2025-04-06 16:39:32 +0200

    Update DiaDia.java

--- src/it/uniroma3/diadia/DiaDia.java
+++ src/it/uniroma3/diadia/DiaDia.java
@@ -1,6 +1,7 @@
 package it.uniroma3.diadia;
 
-import java.util.Scanner;
+import it.uniroma3.diadia.ambienti.Stanza;
+import it.uniroma3.diadia.attrezzi.Attrezzo;
 
 /**
  * Classe principale di diadia, un semplice gioco di ruolo ambientato al dia.
