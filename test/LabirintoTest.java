eb3d500c805c6eae0e79772f8a21b00715422a83
dadifr <dav.calise@stud.uniroma3.it>
2025-04-06 16:42:21 +0200

    Update LabirintoTest.java

--- test/LabirintoTest.java
+++ test/LabirintoTest.java
@@ -1,9 +1,9 @@
-package test;
+package it.uniroma3.diadia.ambienti;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 public class LabirintoTest {
 	Labirinto 1;
 	Stanza biblioteca;
