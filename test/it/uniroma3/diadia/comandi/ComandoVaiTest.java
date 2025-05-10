172408f1cdb9607f0b4567f932719cc21fe183ae
dadifr <dav.calise@stud.uniroma3.it>
2025-05-10 14:35:12 +0200

    Update ComandoVaiTest.java

--- test/it/uniroma3/diadia/comandi/ComandoVaiTest.java
+++ test/it/uniroma3/diadia/comandi/ComandoVaiTest.java
@@ -1,12 +1,12 @@
 package it.uniroma3.diadia.comandi;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.Assert.assertEquals;
+import static org.junit.jupiter.Assert.assertNotEquals;
+import static org.junit.jupiter.Assert.assertTrue;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.AfterEach;
+import org.junit.jupiter.BeforeEach;
+import org.junit.jupiter.Test;
 
 import it.uniroma3.diadia.DiaDia;
 import it.uniroma3.diadia.IOConsole;
@@ -22,7 +22,7 @@ public class ComandoVaiTest {
 	private Comando vai;
 	private Partita p;
 	
-	@Before
+	@BeforeEach
 	public void setUp() throws Exception {
 		s1 = new Stanza("aula 1");
 		s2 = new Stanza("aula 2");
@@ -31,7 +31,7 @@ public class ComandoVaiTest {
 		vai.setIo(new IOConsole());
 	}
 
-	@After
+	@AfterEach
 	public void tearDown() throws Exception {
 	}
 
